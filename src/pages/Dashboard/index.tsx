1b1c1e6f071e88f2935c6b0888ba955e8cf5d455
VeigaThiago <thiagoveiiga@gmail.com>
2020-10-06 17:17:20 -0300

    Challenge finalized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -30,15 +30,28 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
-    async function loadTransactions(): Promise<void> {
-      // TODO
-    }
+    api.get('/transactions').then(response => {
 
-    loadTransactions();
+      const transactionsFormatted = response.data.transactions.map(
+        (transaction: Transaction) => ({
+          ...transaction,
+          formattedValue: formatValue(transaction.value)
+        })
+      )
+
+      const balanceFormatted = {
+        income: formatValue(response.data.balance.income),
+        outcome: formatValue(response.data.balance.outcome),
+        total: formatValue(response.data.balance.total)
+      }
+
+      setTransactions(transactionsFormatted);
+      setBalance(balanceFormatted);
+    })
   }, []);
 
   return (
@@ -51,50 +64,56 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
         <TableContainer>
-          <table>
-            <thead>
-              <tr>
-                <th>Título</th>
-                <th>Preço</th>
-                <th>Categoria</th>
-                <th>Data</th>
-              </tr>
-            </thead>
+          {transactions.map(transaction => (
+            <table>
+              <thead>
+                <tr>
+                  <th>Título</th>
+                  <th>Preço</th>
+                  <th>Categoria</th>
+                  <th>Data</th>
+                </tr>
+              </thead>
 
-            <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
-            </tbody>
-          </table>
+              <tbody>
+                {transaction.type === 'income' && (
+                  <tr>
+                    <td className="title">{transaction.title}</td>
+                    <td className="income">{transaction.formattedValue}</td>
+                    <td>{transaction.category.title}</td>
+                    <td>{transaction.created_at}</td>
+                  </tr>
+                )}
+                {transaction.type === 'outcome' && (
+                  <tr>
+                    <td className="title">{transaction.title}</td>
+                    <td className="outcome">- {transaction.formattedValue}</td>
+                    <td>{transaction.category.title}</td>
+                    <td>{transaction.created_at}</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          ))}
         </TableContainer>
       </Container>
     </>
